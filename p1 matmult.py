7785e685d978d1452391899fdbeed4c7b6e96a94
GabrielGalazzi <gabrielog@al.insper.edu.br>
2023-09-12 12:49:44 -0300

    tentativa de plotar grafico da função

--- p1 matmult.py
+++ p1 matmult.py
@@ -1,8 +1,9 @@
 import math
+import matplotlib
 começo = 0
 i = começo
-fim = 4*math.pi
-tolerancia = 0.00001
+fim = 2*math.pi
+tolerancia = 1*10**(-30)
 auto_intersecao = 0
 coordenadas_autointerseção = []
 pontos_interseção_x = []
@@ -32,7 +33,7 @@ while i < fim:
 
 for j in lista_pontos:
     for h in lista_pontos:
-        if ((j == h and lista_pontos.index(j) != lista_pontos.index(h))) or ((((j[0] == h[0] + tolerancia and j[1] == h[1] + tolerancia) or (j[0] == h[0] - tolerancia and j[1] == h[1] - tolerancia)) and lista_pontos.index(j) != lista_pontos.index(h))):
+        if ((j == h and lista_pontos.index(j) != lista_pontos.index(h))) or (((((j[0] == h[0] + tolerancia and j[1] == h[1] + tolerancia) or (j[0] == h[0] - tolerancia and j[1] == h[1] - tolerancia))or (j[0] == h[0] - tolerancia and j[1] == h[1] + tolerancia) or (j[0] == h[0] + tolerancia and j[1] == h[1] - tolerancia)) and lista_pontos.index(j) != lista_pontos.index(h))):
             #print('BRUH')
             #ajeitar tolerancia (diminuir)
             #encrontrar um jeito de implantar ambas tolerancias (+- tolerancia) ao mesmo tempo (ver codigo de chava da prova de dessof 2021.2)
@@ -41,11 +42,15 @@ for j in lista_pontos:
 set = set(tuple(row) for row in lista_pontos) # por equanto não funciona pois perde-se a ordem dos pontos, mas em teoria isso deixa os pontos unicos na lista de coordenadas.
 coordenadas_autointerseção_prontas = list(set)
 auto_intersecao = len(coordenadas_autointerseção_prontas)
+x_plot = lista_pontos[:,0]
+y_plot = lista_pontos[:,1]
+matplotlib.plot(x_plot,y_plot)
+matplotlib.show()
             
 
-print('ACABOU')
+#print('ACABOU')
 #print(coordenadas_autointerseção_prontas)
-print(auto_intersecao)
+#print(auto_intersecao)
 
 
 
