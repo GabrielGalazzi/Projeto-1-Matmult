f6bd0049fc6665f11bc48956a7b98f2a00296422
GabrielGalazzi <gabrielog@al.insper.edu.br>
2023-09-12 11:17:25 -0300

    Minor changes and improvements

--- p1 matmult.py
+++ p1 matmult.py
@@ -2,6 +2,7 @@ import math
 começo = 0
 i = começo
 fim = 4*math.pi
+tolerancia = 0.00001
 auto_intersecao = 0
 coordenadas_autointerseção = []
 pontos_interseção_x = []
@@ -13,13 +14,13 @@ coordenadas_autointerseção_prontas =[]
 
 def gama_x(t):
 
-    x = 5*math.cos(math.radians(t)) - 4*math.cos(math.radians((5*t)/2))
+    x = 8*math.cos(math.radians(t)) - 5*math.cos(math.radians((4*t)))
 
     return x
 
 def gama_y(t):
 
-    y = 5*math.sin(math.radians(t)) - 4*math.sin(math.radians((5*t)/2))
+    y = 8*math.sin(math.radians(t)) - 5*math.sin(math.radians((4*t)))
 
     return y
 
@@ -31,11 +32,13 @@ while i < fim:
 
 for j in lista_pontos:
     for h in lista_pontos:
-        if j == h and lista_pontos.index(j) != lista_pontos.index(h):
-            print('BRUH')
+        if ((j == h and lista_pontos.index(j) != lista_pontos.index(h))) or ((((j[0] == h[0] + tolerancia and j[1] == h[1] + tolerancia) or (j[0] == h[0] - tolerancia and j[1] == h[1] - tolerancia)) and lista_pontos.index(j) != lista_pontos.index(h))):
+            #print('BRUH')
+            #ajeitar tolerancia (diminuir)
+            #encrontrar um jeito de implantar ambas tolerancias (+- tolerancia) ao mesmo tempo (ver codigo de chava da prova de dessof 2021.2)
             coordenadas_autointerseção.append(j)
 
-set = set(tuple(row) for row in lista_pontos)
+set = set(tuple(row) for row in lista_pontos) # por equanto não funciona pois perde-se a ordem dos pontos, mas em teoria isso deixa os pontos unicos na lista de coordenadas.
 coordenadas_autointerseção_prontas = list(set)
 auto_intersecao = len(coordenadas_autointerseção_prontas)
             
